f9a756663c91c05d2a1632578d5488eae9a9c625
Sahib <masad43255234@gmail.com>
2025-11-24 04:21:14 +0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,5 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-?>
 <script>
     alert("You have been logged out successfully.");
-    window.location.href = "../index.php"; 
+    window.location.href = "index.php"; 
 </script>
+
